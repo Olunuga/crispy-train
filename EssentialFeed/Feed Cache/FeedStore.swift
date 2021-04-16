7f737078fe7cc35e101c0b203ee04327369a6000
Olunuga <backupbox86@gmail.com>
2021-04-16 06:02:44 +0100

    Move `LocalFeedItem`  to its own file

--- EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -15,18 +15,3 @@ public protocol FeedStore {
     func insert(_ items : [LocalFeedItem], timeStamp : Date, completion : @escaping InsertionCompletion)
 }
    
-
-public struct LocalFeedItem : Equatable {
-   public let id : UUID
-   public let description : String?
-   public let location : String?
-   public let imageURL : URL
-    
-    
-    public init(id : UUID, description : String?, location : String?, imageUrl : URL){
-        self.id = id
-        self.description = description
-        self.location = location
-        self.imageURL = imageUrl
-    }
-}
---
+++ EssentialFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,23 @@
+//
+//  LocalFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Mayowa Olunuga on 16/04/2021.
+//
+
+import Foundation
+
+public struct LocalFeedItem : Equatable {
+   public let id : UUID
+   public let description : String?
+   public let location : String?
+   public let imageURL : URL
+    
+    
+    public init(id : UUID, description : String?, location : String?, imageUrl : URL){
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageUrl
+    }
+}
