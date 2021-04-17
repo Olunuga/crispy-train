c84c2f1d227bc1709ec9d9ceb8f50079c8200a64
Olunuga <backupbox86@gmail.com>
2021-04-17 15:40:34 +0100

    ValidateFeedCache does not message store on creation

---
+++ EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -0,0 +1,28 @@
+//
+//  ValidateFeedCacheUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Mayowa Olunuga on 17/04/2021.
+//
+
+import XCTest
+import EssentialFeed
+
+class ValidateFeedCacheUseCaseTests : XCTestCase {
+    func test_init_doesNotMessageUponCreation(){
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    
+    
+    //MARK: Helper
+    private func makeSUT(currentDate : @escaping ()->Date = Date.init, file: StaticString = #filePath, line: UInt = #line)-> (sut: LocalFeedLoader, store : FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate : currentDate)
+        trackForMemoryLeak(sut, file: file, line:  line)
+        trackForMemoryLeak(store, file: file, line: line)
+        return (sut, store)
+    }
+}
