3aeb49ff10e9b7b9c88949fc08c4968bad168f1c
Olunuga <backupbox86@gmail.com>
2021-04-29 09:02:11 +0100

    Extract reusable CoreData helpers into a separate file

--- EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -65,37 +65,6 @@ public final class CoreDataFeedStore : FeedStore {
 }
 
 
-extension NSPersistentContainer {
-    enum LoadingError: Swift.Error {
-        case modelNotFound
-        case failedToLoadPersistentStores(Swift.Error)
-    }
-    
-    static func load(storeURL : URL, modelName : String, in bundle : Bundle) throws -> NSPersistentContainer {
-        guard let model = NSManagedObjectModel.with(name: modelName, in: bundle) else {
-            throw LoadingError.modelNotFound
-        }
-        
-        let description = NSPersistentStoreDescription(url: storeURL)
-        let container = NSPersistentContainer(name: modelName, managedObjectModel: model)
-        container.persistentStoreDescriptions = [description]
-        
-        var loadError : Swift.Error?
-        container.loadPersistentStores{ loadError = $1}
-        try loadError.map { throw LoadingError.failedToLoadPersistentStores($0)}
-        
-        return container
-        
-    }
-}
-
-extension NSManagedObjectModel {
-    static func with(name : String, in bundle :Bundle) -> NSManagedObjectModel? {
-        return bundle.url(forResource: name, withExtension: "momd").flatMap{NSManagedObjectModel(contentsOf: $0)}
-    }
-}
-
-
 @objc(ManagedCache)
 private class ManagedCache : NSManagedObject {
     @NSManaged var timestamp : Date
