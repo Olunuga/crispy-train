98feca23f777f5e4e3a19192981173bc5b2c62bd
Olunuga <backupbox86@gmail.com>
2021-05-01 12:39:22 +0100

    `LocalFeedLoader` in integration with the `CoreDataFeedStore` delivers no item on empty cache

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -6,9 +6,43 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class EssentialFeedCacheIntegrationTests: XCTestCase {
 
-   
+    func test_local_deliversNoItemOnEmptyCache(){
+        let sut = makeSUT()
+        
+        let exp = expectation(description: "Wait for load")
+        sut.load{ result in
+            switch result {
+            case let .success(imageFeed):
+                XCTAssertEqual(imageFeed, [], "Expected empty feed")
+            case let .failure(error):
+                XCTFail("Expected successful feed result, but got \(error) ")
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func makeSUT(file : StaticString = #filePath, line : UInt = #line)-> LocalFeedLoader {
+        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+        let storeUrl = testSpecificStoreURL()
+        let store = try! CoreDataFeedStore(storeURL: storeUrl, bundle: storeBundle)
+        let sut = LocalFeedLoader(store: store, currentDate: Date.init)
+        trackForMemoryLeak(sut, file:file, line : line)
+        trackForMemoryLeak(sut, file:file, line : line)
+        return sut
+    }
+    
+    func testSpecificStoreURL() -> URL {
+        return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
+    }
+    
+    func cachesDirectory() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
 
 }
