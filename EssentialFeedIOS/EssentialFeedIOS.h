579784d136ac2980fb8aa005520a30cb79134030
Olunuga <backupbox86@gmail.com>
2021-05-09 20:18:57 +0100

    Add `EssentialFeedIOS` Framework (Prod and Test) targets for IOS platform-specific components

---
+++ EssentialFeedIOS/EssentialFeedIOS.h
@@ -0,0 +1,18 @@
+//
+//  EssentialFeedIOS.h
+//  EssentialFeedIOS
+//
+//  Created by Mayowa Olunuga on 09/05/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeedIOS.
+FOUNDATION_EXPORT double EssentialFeedIOSVersionNumber;
+
+//! Project version string for EssentialFeedIOS.
+FOUNDATION_EXPORT const unsigned char EssentialFeedIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeedIOS/PublicHeader.h>
+
+
