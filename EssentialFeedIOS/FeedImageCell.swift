48e4c1b3583f6fb444ae96c8198d07526c65ab44
Olunuga <backupbox86@gmail.com>
2021-05-11 07:44:09 +0100

    Render loaded feed on successful load completion

---
+++ EssentialFeedIOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeedIOS
+//
+//  Created by Mayowa Olunuga on 11/05/2021.
+//
+
+import UIKit
+
+final public class FeedImageCell : UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
