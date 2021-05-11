f86972d0da863fb297c6b051fe89a7de6a8ea609
Olunuga <backupbox86@gmail.com>
2021-05-11 13:37:43 +0100

    Retry failed image load on retry action

--- EssentialFeedIOSTests/FeedViewControllerTests.swift
+++ EssentialFeedIOSTests/FeedViewControllerTests.swift
@@ -201,6 +201,29 @@ final class FeedViewControllerTests : XCTestCase {
         
     }
     
+    func test_feedImageViewRetryAction_retriesImageLoad(){
+        let image0 = makeImage(url: URL(string: "http://url-0.com")!)
+        let image1 = makeImage(url: URL(string: "http://url-1.com")!)
+        let (sut, loader) = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        loader.completeFeedLoading(with: [image0, image1])
+        
+        let view0 = sut.simulateFeedImageViewVisible(at: 0)
+        let view1 = sut.simulateFeedImageViewVisible(at: 1)
+        XCTAssertEqual(loader.loadedImageURLS, [image0.url, image1.url], "Expected two image url requests for the two visible views")
+        
+        loader.completeImageLoadingWithError(at: 0)
+        loader.completeImageLoadingWithError(at: 1)
+        XCTAssertEqual(loader.loadedImageURLS, [image0.url, image1.url], "Expected only two image URL requests before retry action")
+        
+        view0?.simulateRetryAction()
+        XCTAssertEqual(loader.loadedImageURLS, [image0.url, image1.url, image0.url], "Expected third image url requests after first retry action")
+        
+        view1?.simulateRetryAction()
+        XCTAssertEqual(loader.loadedImageURLS, [image0.url, image1.url, image0.url, image1.url], "Expected fourth image url requests after first retry action")
+    }
+    
     //MARK: - Helpers
     
     private func makeSUT(file : StaticString = #filePath, line : UInt = #line) -> (sut : FeedViewController, loader : LoaderSpy) {
@@ -348,6 +371,16 @@ private extension UIRefreshControl {
     }
 }
 
+private extension UIButton {
+    func simulateTap(){
+        allTargets.forEach{ target in
+            actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach{
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+    }
+}
+
 
 private extension FeedImageCell {
     var isShowingLocation : Bool {
@@ -372,6 +405,10 @@ private extension FeedImageCell {
     
     var renderedImage : Data? {
        return feedImageView.image?.pngData()
+    }
+    
+    func simulateRetryAction(){
+        feedImageRetryButton.simulateTap()
     }
 }
 
