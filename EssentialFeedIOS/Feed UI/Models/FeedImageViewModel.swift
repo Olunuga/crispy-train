dc2f233daafb414f9f6c7eb35e316f888883cc9a
Olunuga <backupbox86@gmail.com>
2021-05-12 23:28:45 +0100

    Decouple FeedImageViewModel from UIKit by creating a transformation closure that converts an image `Data` value into a generic `Image` type. When composing with a UIKit user interface, we inject a closure to transform the image `Data` into `UIImage`.

---
+++ EssentialFeedIOS/Feed UI/Models/FeedImageViewModel.swift
@@ -0,0 +1,56 @@
+//
+//  FeedImageViewModel.swift
+//  EssentialFeedIOS
+//
+//  Created by Mayowa Olunuga on 12/05/2021.
+//
+
+import EssentialFeed
+
+final public class FeedImageViewModel<Image> {
+    typealias Observable<T> = (T)->Void
+    
+    private var task : FeedImageDataLoaderTask?
+    private var model : FeedImage
+    private var imageLoader : FeedImageDataLoader
+    private let imageTransformer : (Data) -> Image?
+    
+    init(model : FeedImage, imageLoader : FeedImageDataLoader, imageTransformer : @escaping (Data) -> Image?) {
+        self.model = model
+        self.imageLoader = imageLoader
+        self.imageTransformer = imageTransformer
+    }
+    
+    var hasLocation : Bool  {model.location != nil}
+    var location : String? {model.location}
+    var description : String? {model.description}
+    
+    var onImageLoad : Observable<Image>?
+    var onImageLoadingStateChange : Observable<Bool>?
+    var onShouldRetryImageLoadStateChange : Observable<Bool>?
+    
+    func loadImageData(){
+        onImageLoadingStateChange?(true)
+        onShouldRetryImageLoadStateChange?(false)
+        task = imageLoader.loadImageData(from : model.url) {[weak self] result in
+            self?.handle(result)
+        }
+    }
+    
+    private func handle(_ result : FeedImageDataLoader.Result){
+        if let image = (try? result.get()).flatMap(imageTransformer) {
+            onImageLoad?(image)
+        }else{
+            onShouldRetryImageLoadStateChange?(true)
+        }
+        onImageLoadingStateChange?(false)
+    }
+    
+    func preLoad(){
+        self.task = self.imageLoader.loadImageData(from : self.model.url) {_ in }
+    }
+    
+    func cancelLoad() {
+        task?.cancel()
+    }
+}
